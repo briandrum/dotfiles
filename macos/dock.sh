459c84896b87a745a593aa9a26b56a1794610723
Brian Drum <brian@briandrum.net>
2021-05-12 14:00:03 -0400

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Things3.app"
+dockutil --no-restart --add "/Applications/Notion.app"
+dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/Zoom.us.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/BBEdit.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Music.app"
+dockutil --no-restart --add "/System/Applications/Music.app"
 
 killall Dock
